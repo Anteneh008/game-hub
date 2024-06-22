2cf4e0e2529729f9e4c2d5af1d09e30da84790ea
Anteneh008 <anteneh.atre@gmail.com>
2024-06-22 14:48:01 +0300

    Handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./Pages/ErrorPage";
 import GameDetailPage from "./Pages/GameDetailPage";
 import HomePage from "./Pages/HomePage";
 import Layout from "./Pages/Layout";
@@ -7,6 +8,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: "games/:id", element: <GameDetailPage /> },
